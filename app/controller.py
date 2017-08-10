c29cb9cdcf5eac5b43da49f709a2439156c6097f
vbover <vbover@163.com>
2017-08-10 23:42:46 +0800

    [modify] ControlMainWindow

--- app/controller.py
+++ app/controller.py
@@ -1,1 +1,18 @@
-#coding = utf-8
+# coding = utf-8
+from PyQt5 import QtWidgets
+from gui.main_window import Ui_MainWindow
+
+
+class ControlMainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
+    def __init__(self):
+        super(ControlMainWindow,self).__init__()
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    main_window=ControlMainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
