e44619510206017b28c00647b8532c1d09594657
vbover <vbover@163.com>
2017-08-12 19:26:09 +0800

    [modify] global variable

--- app/global_list.py
+++ app/global_list.py
@@ -1,4 +1,12 @@
 # coding = utf-8
 
 TOTAL_PLAYER = 0
+USER_DB = {}
 NOW_ROUND = 0
+NOW_PLAYER = 0
+ROLE_TYPE_LIST = []
+
+
+def set_now_player(value: int):
+    global NOW_PLAYER
+    NOW_PLAYER = value
