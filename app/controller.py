cb10bade1cc43948fcb53161e5101371982c45f0
vbover <vbover@163.com>
2017-08-18 07:56:41 +0800

    [modify] add left_button function

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,5 @@
 # coding = utf-8
-from PyQt5.QtWidgets import QWidget, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QWidget, QMainWindow, QMessageBox, QListWidget
 from gui.main_window import Ui_MainWindow
 from gui.game_set_form import Ui_GameSetForm
 import app.global_list
@@ -11,9 +11,9 @@ class ControlMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(ControlMainWindow, self).__init__()
         self.setupUi(self)
-        self.new_game = ControlGameSetForm()
         global main_win
         main_win = self
+        self.new_game = ControlGameSetForm()
         total_player = app.global_list.TOTAL_PLAYER
 
         for mid in range(total_player):
@@ -77,8 +77,58 @@ class ControlGameSetForm(QWidget, Ui_GameSetForm):
         self.setupUi(self)
         self.total_player = self.totalSetSpinBox.value()
         self.select_role = []
+        self.add_roles = []
+        self.init_role_list()
+        self.init_button_connect()
 
-        self.determineButton.clicked.connect(self.close)
+    def init_button_connect(self):
+        def click_default_button():
+            self.allRoleList.clear()
+            self.init_all_role_list()
+            self.select_role.clear()
+
+        def click_determine_button():
+            if not self.save_option():
+                msg = QMessageBox()
+                msg.setWindowTitle('设置错误')
+                msg.setText('没有选择玩家类型！')
+                msg.setIcon(QMessageBox.Information)
+                msg.addButton('确定', QMessageBox.YesRole)
+                msg.exec()
+            else:
+                self.close()
+
+        def click_right_button():
+            role_list = self.allRoleList.selectedItems()
+            for item in role_list:
+                index = self.allRoleList.row(item)
+                item = self.allRoleList.takeItem(index)
+
+                self.select_role.append(item.text())
+                self.selectRoleList.addItem(item)
+                self.allRoleList.removeItemWidget(item)
+            print(self.select_role)
+
+        def click_left_button():
+            role_list = self.selectRoleList.selectedItems()
+            for item in role_list:
+                index = self.selectRoleList.row(item)
+                item = self.selectRoleList.takeItem(index)
+
+                self.select_role.remove(item.text())
+                self.allRoleList.insertItem(get_all_type_list().index(item.text()), item)
+                self.selectRoleList.removeItemWidget(item)
+            print(self.select_role)
+
+        self.defaultButton.clicked.connect(click_default_button)
+        self.determineButton.clicked.connect(click_determine_button)
+        self.rightButton.clicked.connect(click_right_button)
+        self.leftButton.clicked.connect(click_left_button)
+
+    def init_role_list(self):
+        self.allRoleList.setSelectionMode(QListWidget.MultiSelection)
+        self.allRoleList.addItems(get_all_type_list())
+        self.selectRoleList.setSelectionMode(QListWidget.MultiSelection)
 
     def save_option(self):
         if not self.select_role:
@@ -88,19 +138,7 @@ class ControlGameSetForm(QWidget, Ui_GameSetForm):
         return True
 
     def closeEvent(self, close_event):
-        sender = self.sender()
-        flag = True
-        if sender:
-            if not self.save_option():
-                msg = QMessageBox()
-                msg.setWindowTitle('设置错误')
-                msg.setText('没有选择玩家类型！')
-                msg.setIcon(QMessageBox.Information)
-                msg.addButton('确定', QMessageBox.YesRole)
-                msg.exec()
-                close_event.ignore()
-                flag = False
-        elif self.select_role:
+        if self.select_role:
             msg = QMessageBox()
             msg.setWindowTitle('保存')
             msg.setText('是否保存设置？')
@@ -110,5 +148,14 @@ class ControlGameSetForm(QWidget, Ui_GameSetForm):
             reply = msg.exec()
             if reply == QMessageBox.YesRole:
                 self.save_option()
-        if flag and main_win:
+        if main_win:
             main_win.show()
+
+
+def get_all_type_list():
+    special_role = app.global_list.SPECIAL_ROLE
+    result = []
+    for key in app.global_list.ROLE_STYLE.keys():
+        if key not in special_role:
+            result.append(key)
+    return result
