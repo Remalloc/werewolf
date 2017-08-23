391514db2572dc08445f7b6ff2e984a330613047
vbover <vbover@163.com>
2017-08-23 21:47:15 +0800

    [modify] add get_set fucation

--- app/global_list.py
+++ app/global_list.py
@@ -5,13 +5,14 @@ import gui.img
 PATH_CONFIG = join('.', 'config.json')
 
 TOTAL_PLAYER = 0
+RECORD_VOTE = []
 USER_DB = {}
 
 NOW_ROUND = 0
 NOW_PLAYER = 0
 
-ROLE_TYPE_LIST = []
-ALL_ROLE=['狼人','村民','预言家','丘比特','猎人','白痴','守卫','盗贼','村长','白狼王','女巫']
+ROLE_TYPE = []
+ALL_ROLE = ['狼人', '村民', '预言家', '丘比特', '猎人', '白痴', '守卫', '盗贼', '村长', '白狼王', '女巫']
 ACTION_TYPE = ("明捞", "暗捞", "重踩", "轻踩")
 
 DEFAULT_STYLE = 'border-image:url(%s);border-radius:10px;'
@@ -26,9 +27,8 @@ ROLE_STYLE = {'狼人': DEFAULT_STYLE % ':/img/狼人',
               '村长': DEFAULT_STYLE % ':/img/村长',
               '白狼王': DEFAULT_STYLE % ':/img/白狼王',
               '女巫': DEFAULT_STYLE % ':/img/女巫',
-              '警长': DEFAULT_STYLE % ':/img/警长',
               '未知': DEFAULT_STYLE % ':/img/未知',
-              '自定义':DEFAULT_STYLE % ':/img/自定义'
+              '自定义': DEFAULT_STYLE % ':/img/自定义'
               }
 
 
@@ -53,6 +53,67 @@ def read_config():
                 else:
                     all_var[var] = value
 
-def set_now_player(player: int):
+
+def get_total_player():
+    return TOTAL_PLAYER
+
+
+def set_total_player(total: int):
+    global TOTAL_PLAYER
+    TOTAL_PLAYER = total
+
+
+def get_now_player():
+    return NOW_PLAYER
+
+
+def set_now_player(mid: int):
     global NOW_PLAYER
-    NOW_PLAYER = player
+    NOW_PLAYER = mid
+
+
+def get_user_db(user: int):
+    return USER_DB.get(user)
+
+
+def add_user_db(mid: int, user):
+    global USER_DB
+    USER_DB[mid] = user
+    return mid
+
+
+def clear_user_db():
+    global USER_DB
+    USER_DB.clear()
+
+
+def get_role_type():
+    return ROLE_TYPE
+
+
+def set_role_type(role_type: list):
+    global ROLE_TYPE
+    ROLE_TYPE = role_type
+
+
+def get_all_role():
+    return ALL_ROLE
+
+
+def add_all_role(role: str):
+    ALL_ROLE.append(role)
+    return role
+
+
+def get_role_style(role: str):
+    return ROLE_STYLE.get(role)
+
+
+def get_now_round():
+    return NOW_ROUND
+
+
+def add_now_round():
+    global NOW_ROUND
+    NOW_ROUND += 1
+    return get_now_round()
